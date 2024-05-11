5934fc465f2d346598de8834d006004f4fb8dbf4
Moin Shaikh <moin241397@gmail.comm>
2024-05-11 22:23:33 +0530

    hello

---
+++ src/main/java/com/example/devopsms/DevopsMsApplication.java
@@ -0,0 +1,13 @@
+package com.example.devopsms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/devopsms/DevopsMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.devopsms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
